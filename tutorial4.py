0311ecb0d5378268b89b998412e8f274ac162087
esHathck05 <46973649+esHathck05@users.noreply.github.com>
2019-03-26 14:44:45 -0400

    Updated from Brython Server: 3/26/2019 2:44:43 PM

--- tutorial4.py
+++ tutorial4.py
@@ -1,9 +1,6 @@
 from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
 
 class SpaceShip(Sprite):
-    """
-    Animated space ship
-    """
     asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", 
         Frame(227,0,65,125), 4, 'vertical')
 
@@ -18,23 +15,23 @@ class SpaceShip(Sprite):
         SpaceGame.listenKeyEvent("keyup", "space", self.thrustOff)
         self.fxcenter = self.fycenter = 0.5
     
-        def step(self):
-            self.x += self.vx
-            self.y += self.vy
-            self.rotation += self.vr
-             # manage thrust animation
-            if self.thrust == 1:
-                self.setImage(self.thrustframe)
-                self.thrustframe += 1
-                if self.thrustframe == 4:
-                    self.thrustframe = 1
-            else:
-                self.setImage(0)
+    def step(self):
+        self.x += self.vx
+        self.y += self.vy
+        self.rotation += self.vr
+        # manage thrust animation
+        if self.thrust == 1:
+            self.setImage(self.thrustframe)
+            self.thrustframe += 1
+            if self.thrustframe == 4:
+                self.thrustframe = 1
+        else:
+            self.setImage(0)
 
-        def thrustOn(self, event):
+    def thrustOn(self, event):
             self.thrust = 1
         
-        def thrustOff(self, event):
+    def thrustOff(self, event):
             self.thrust = 0
 
 class SpaceGame(App):
