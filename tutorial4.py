1f3919b89ef2e847de4b3faba9e91780828fe107
esHathck05 <46973649+esHathck05@users.noreply.github.com>
2019-03-26 14:42:13 -0400

    Updated from Brython Server: 3/26/2019 2:42:10 PM

--- tutorial4.py
+++ tutorial4.py
@@ -1,5 +1,42 @@
 from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
 
+class SpaceShip(Sprite):
+    """
+    Animated space ship
+    """
+    asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", 
+        Frame(227,0,65,125), 4, 'vertical')
+
+    def __init__(self, position):
+        super().__init__(SpaceShip.asset, position)
+        self.vx = 1
+        self.vy = 1
+        self.vr = 0.01
+        self.thrust = 0
+        self.thrustframe = 1
+        SpaceGame.listenKeyEvent("keydown", "space", self.thrustOn)
+        SpaceGame.listenKeyEvent("keyup", "space", self.thrustOff)
+        self.fxcenter = self.fycenter = 0.5
+    
+        def step(self):
+            self.x += self.vx
+            self.y += self.vy
+            self.rotation += self.vr
+             # manage thrust animation
+            if self.thrust == 1:
+                self.setImage(self.thrustframe)
+                self.thrustframe += 1
+                if self.thrustframe == 4:
+                    self.thrustframe = 1
+            else:
+                self.setImage(0)
+
+        def thrustOn(self, event):
+            self.thrust = 1
+        
+        def thrustOff(self, event):
+            self.thrust = 0
+
 class SpaceGame(App):
     """
     Tutorial4 space game example.
@@ -11,6 +48,13 @@ class SpaceGame(App):
         noline = LineStyle(0, black)
         bg_asset = RectangleAsset(self.width, self.height, noline, black)
         bg = Sprite(bg_asset, (0,0))
+        SpaceShip((100,100))
+        SpaceShip((150,150))
+        SpaceShip((200,50))
+        
+    def step(self):
+        for ship in self.getSpritesbyClass(SpaceShip):
+            ship.step()
 
 myapp = SpaceGame()
 
